c7ab6baea8d1a5523087cf063a547632d531e3b1
Michele Locati <michele@locati.it>
2014-08-27 14:05:32 +0200

    Switch from phpDocumentor to ApiGen

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Punic\\Calendar"],["c","Punic\\Data"],["c","Punic\\Exception"],["c","Punic\\Exception\\BadArgumentType"],["c","Punic\\Exception\\BadDataFileContents"],["c","Punic\\Exception\\DataFileNotFound"],["c","Punic\\Exception\\DataFileNotReadable"],["c","Punic\\Exception\\DataFolderNotFound"],["c","Punic\\Exception\\InvalidDataFile"],["c","Punic\\Exception\\InvalidLocale"],["c","Punic\\Exception\\NotImplemented"],["c","Punic\\Exception\\ValueNotInList"],["c","Punic\\Misc"],["c","Punic\\Plural"],["c","Punic\\Unit"]];
